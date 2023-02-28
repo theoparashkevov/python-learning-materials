322e97451589ed787e48377a12313b692c4048c0
theoparashkevov <theo.parashkevov@gmail.com>
2023-02-28 10:10:18 +0200

    [List] Created Examples for remove()

---
+++ Beginner Python/lists/example_remove.py
@@ -0,0 +1,8 @@
+example_list = [1, 2, 2, 3, 3, 3, 'a', 'b', 'c', 'X', 'Y', 'Z']
+print(example_list, '\n')
+
+example_list.remove(2)
+print(example_list, '\n')
+
+example_list.remove('a')
+print(example_list, '\n')
