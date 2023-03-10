330e89c718726c73ff097a02f72c4774810030a0
theoparashkevov <theo.parashkevov@gmail.com>
2023-03-10 19:39:09 +0200

    [Dictionary] Created Examples for keys()

---
+++ Beginner Python/dictionaries/example_keys.py
@@ -0,0 +1,5 @@
+example_dictionary = {1: 'str_1', 2: 'str_2', 3: 'str_3', '3': 3, '4': 4, '5': 5}
+print(example_dictionary)
+
+for key in example_dictionary.keys():
+    print(key, type(key), sep='\t\t')
