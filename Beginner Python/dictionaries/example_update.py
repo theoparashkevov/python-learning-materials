15067bd35aa98a123b843478a2cd7f3dd53c61f9
theoparashkevov <theo.parashkevov@gmail.com>
2023-03-10 19:39:19 +0200

    [Dictionary] Created Examples for update()

---
+++ Beginner Python/dictionaries/example_update.py
@@ -0,0 +1,7 @@
+example_dictionary = {1: 'str_1', 2: 'str_2', 3: 'str_3', '3': 3, '4': 4, '5': 5}
+print(example_dictionary)
+
+dictionary_2 = {7:'7'}
+example_dictionary.update(dictionary_2)
+print(example_dictionary)
+
