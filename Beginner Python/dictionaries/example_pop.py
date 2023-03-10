03eefccc6aadcdd4249627677691016e6ee91912
theoparashkevov <theo.parashkevov@gmail.com>
2023-03-10 19:39:36 +0200

    [Dictionary] Created Examples for pop()

---
+++ Beginner Python/dictionaries/example_pop.py
@@ -0,0 +1,8 @@
+example_dictionary = {1: 'str_1', 2: 'str_2', 3: 'str_3', '3': 3, '4': 4, '5': 5}
+print(example_dictionary)
+
+
+popped_val_1 = example_dictionary.pop(1)
+print('Value: ', popped_val_1)
+
+print(example_dictionary)
