6f0ddf631d6e81aba54d3957f47ee0ffbac19a16
theoparashkevov <theo.parashkevov@gmail.com>
2023-03-10 19:38:37 +0200

    [Dictionary] Created Examples for clear()

---
+++ Beginner Python/dictionaries/example_clear.py
@@ -0,0 +1,6 @@
+example_dictionary = {1: 'str_1', 2: 'str_2', 3: 'str_3', '3': 3, '4': 4, '5': 5}
+print(example_dictionary)
+
+# Empties the dictionary
+example_dictionary.clear()
+print(example_dictionary)
