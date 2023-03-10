ef3db6f96391d201b5f6248c182a043b7c2ec19e
theoparashkevov <theo.parashkevov@gmail.com>
2023-03-10 19:40:07 +0200

    [List] Updated Examples for extend()

--- Beginner Python/lists/example_extend.py
+++ Beginner Python/lists/example_extend.py
@@ -3,3 +3,8 @@ print(example_list)
 
 example_list.extend([11, 22, 33])
 print(example_list)
+
+# Will extend using first the values and the keys of the dictionary
+example_list.extend({1: '11', 2: '22', 3: '33'})
+print(example_list)
+
