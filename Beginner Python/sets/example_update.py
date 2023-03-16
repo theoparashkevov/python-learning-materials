bf08315042578180de729912ddc71a068ca68eaf
theoparashkevov <theo.parashkevov@gmail.com>
2023-03-16 16:41:10 +0200

    [Set] Updated Examples for update()

---
+++ Beginner Python/sets/example_update.py
@@ -0,0 +1,11 @@
+# Create a set
+my_set = {1, 2, 3}
+
+# Define a list of new elements to add to the set
+new_elements = [4, 5, 6]
+
+# Use the update() function to add the new elements to the set
+my_set.update(new_elements)
+
+# Print the updated set
+print(my_set)
