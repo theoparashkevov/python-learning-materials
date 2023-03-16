1eb5cccca56c745a94a2ff8b98d3e8fb9fbf9fc7
theoparashkevov <theo.parashkevov@gmail.com>
2023-03-16 17:33:10 +0200

    [Set] Updated Examples for __format__()

---
+++ Beginner Python/sets/magic/example___format__.py
@@ -0,0 +1,19 @@
+# Define a custom class that inherits from set
+class CustomSet(set):
+	# Override the __format__ method to return a custom string representation
+	def __format__(self, format_spec):
+
+		# Format the set using the built-in str.format() method
+		formatted_set = str(self).replace('{', '{{').replace('}', '}}')
+
+		# Return the formatted string
+		return formatted_set.__format__(format_spec)
+
+
+# Create a CustomSet object
+my_set = CustomSet([1, 2, 3])
+
+# Use the format() function to format the set with a custom format string
+formatted_set = format(my_set, '')
+print('Formatted Set => ', formatted_set)
+
