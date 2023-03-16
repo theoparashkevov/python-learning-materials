1150b8174ad30d0ffe2ab3cf55bec3c882bd9e12
theoparashkevov <theo.parashkevov@gmail.com>
2023-03-16 16:39:12 +0200

    [Set] Updated Examples for clear()

---
+++ Beginner Python/sets/example_clear.py
@@ -0,0 +1,11 @@
+# Create a set
+my_set = {1, 2, 3, 4, 5}
+
+# Display the set
+print("Original set:", my_set)
+
+# Clear the set
+my_set.clear()
+
+# Display the set after clearing
+print("Set after clearing:", my_set)
