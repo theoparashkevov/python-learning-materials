94d5d6279b04e1d315a3703e0916f8be4aa017dd
theoparashkevov <theo.parashkevov@gmail.com>
2023-03-16 17:32:34 +0200

    [Set] Updated Examples for __contains__()

---
+++ Beginner Python/sets/magic/example___contains__.py
@@ -0,0 +1,8 @@
+# Creating a set of colors
+colors = {"red", "green", "blue"}
+
+# Checking if "red" is present in the set using __contains__ function
+if colors.__contains__("red"):
+    print("The set contains red!")
+else:
+    print("The set does not contain red.")
