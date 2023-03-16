2da80fb54d72459c8e61dafd17b4ab6b72e7cb98
theoparashkevov <theo.parashkevov@gmail.com>
2023-03-16 16:40:00 +0200

    [Set] Updated Examples for intersection_update()

---
+++ Beginner Python/sets/example_intersection_update.py
@@ -0,0 +1,9 @@
+# Create two sets
+set1 = {1, 2, 3, 4}
+set2 = {2, 3, 5, 6}
+
+# Use intersection_update to update set1 with the intersection of set1 and set2
+set1.intersection_update(set2)
+
+# Print the updated set1
+print(set1)
