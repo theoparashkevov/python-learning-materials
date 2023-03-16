a7ca1383ca673825c06ba180aaf6c0b414f5bcc1
theoparashkevov <theo.parashkevov@gmail.com>
2023-03-16 16:40:31 +0200

    [Set] Updated Examples for pop()

---
+++ Beginner Python/sets/example_pop.py
@@ -0,0 +1,9 @@
+# create a set
+my_set = {1, 2, 3, 4, 5}
+
+# remove and return a random element from the set
+removed_element = my_set.pop()
+
+# print the removed element and the updated set
+print("Removed element:", removed_element)
+print("Updated set:", my_set)
