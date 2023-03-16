6fbc25465b15e578b09bccd18250836104678e43
theoparashkevov <theo.parashkevov@gmail.com>
2023-03-16 16:39:46 +0200

    [Set] Updated Examples for discard()

---
+++ Beginner Python/sets/example_discard.py
@@ -0,0 +1,8 @@
+# create a set of fruits
+fruits = {"apple", "banana", "cherry", "orange"}
+
+# remove "banana" from the set using discard()
+fruits.discard("banana")
+
+# print the updated set of fruits
+print(fruits)
