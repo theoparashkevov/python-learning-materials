b50b318542707fa2efe4de5e76461fe177bd48b3
theoparashkevov <theo.parashkevov@gmail.com>
2023-03-30 21:25:10 +0300

    [Set] Update __subclasshook__

--- Beginner Python/sets/magic/example___subclasshook__.py
+++ Beginner Python/sets/magic/example___subclasshook__.py
@@ -1,25 +1,26 @@
 from abc import ABCMeta
 from abc import abstractmethod
+
 class Set(metaclass=ABCMeta):
-	@abstractmethod
-	def __contains__(self, element):
-		pass
+    @abstractmethod
+    def __contains__(self, element):
+        pass
 
-	@abstractmethod
-	def __iter__(self):
-		pass
+    @abstractmethod
+    def __iter__(self):
+        pass
 
-	@abstractmethod
-	def __len__(self):
-		pass
+    @abstractmethod
+    def __len__(self):
+        pass
 
-	@classmethod
-	def __subclasshook__(cls, C):
-		if cls is Set:
-			if any("__contains__" in B.__dict__ for B in C.__mro__) and \
-					any("__iter__" in B.__dict__ for B in C.__mro__) and \
-					any("__len__" in B.__dict__ for B in C.__mro__):
-				return True
-		return NotImplemented
+    @classmethod
+    def __subclasshook__(cls, C):
+        if cls is Set:
+            if any("__contains__" in B.__dict__ for B in C.__mro__) and \
+                    any("__iter__" in B.__dict__ for B in C.__mro__) and \
+                    any("__len__" in B.__dict__ for B in C.__mro__):
+                return True
+        return NotImplemented
 
 print(issubclass(set, Set))
