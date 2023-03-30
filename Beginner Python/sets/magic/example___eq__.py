b28c82376770ca5f4c7bfe01f6b5929311860f09
theoparashkevov <theo.parashkevov@gmail.com>
2023-03-30 21:24:50 +0300

    [Set] Update __eq__

--- Beginner Python/sets/magic/example___eq__.py
+++ Beginner Python/sets/magic/example___eq__.py
@@ -1,9 +1,9 @@
 class MySet:
-	def __init__(self, values):
-		self.values = set(values)
+    def __init__(self, values):
+        self.values = set(values)
 
-	def __eq__(self, other):
-		return self.values == other.values
+    def __eq__(self, other):
+        return self.values == other.values
 
 
 set1 = MySet([1, 2, 3])
