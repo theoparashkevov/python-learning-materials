4df16edc72e2d47afa0de598dd96cbab59af4d30
theoparashkevov <theo.parashkevov@gmail.com>
2023-04-06 10:01:00 +0300

    [lists] magic __getattribute__

---
+++ Beginner Python/lists/magic/example___getattribute__.py
@@ -0,0 +1,9 @@
+class MyList(list):
+	def __getattribute__(self, name):
+		print(f"Getting attribute {name}")
+
+		return super().__getattribute__(name)
+
+
+my_list = MyList([1, 2, 3])
+print(my_list.__len__())
