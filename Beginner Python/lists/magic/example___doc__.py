de87859f5a296f7b4e3344c8306cb0bde965b5b2
theoparashkevov <theo.parashkevov@gmail.com>
2023-04-06 10:00:05 +0300

    [lists] magic __doc__

---
+++ Beginner Python/lists/magic/example___doc__.py
@@ -0,0 +1,7 @@
+my_list = [1, 2, 3]
+
+# Print the docstring of my_list using the help() function
+help(my_list)
+
+# Print the docstring of my_list using the .__doc__ attribute
+print(my_list.__doc__)
